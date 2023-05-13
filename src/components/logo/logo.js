dad02b5978b2c133ea9f27f13e128c8474dab448
LizaBinar <106739245+LizaBinar@users.noreply.github.com>
2023-05-14 03:57:16 +0700

    master init

---
+++ src/components/logo/logo.js
@@ -0,0 +1,12 @@
+import logoSVG from "./logo.svg"
+
+const Logo = () => {
+
+    return (
+        <a href="#">
+            <img src={logoSVG} alt={"logo"}/>
+        </a>
+    )
+}
+
+export default Logo
